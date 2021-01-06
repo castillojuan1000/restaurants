cec47efd5995a41a3613cbaf29e41f1f1ff8dbc4
Juan Castillo <castillojuan1000@gmail.com>
2021-01-06 11:37:10 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="container">
+    <Hero />
+    <RestaurantsInfo />
+  </main>
+</template>
+
+<script>
+import Hero from "@/components/Hero.vue";
+import RestaurantsInfo from "@/components/RestaurantsInfo.vue";
+
+export default {
+  components: {
+    Hero,
+    RestaurantsInfo,
+  },
+};
+</script>
+
+<style>
+</style>
